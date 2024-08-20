7ec9c953758705b6e896a41d134ee43a41d29c8f
zhangrui <1243065739@qq.com>
2024-08-20 15:16:13 +0800

    第一次idea提交

---
+++ src/main/java/com/yjx/demogit01/DemoGit01Application.java
@@ -0,0 +1,13 @@
+package com.yjx.demogit01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGit01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGit01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjx/demogit01/DemoGit01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjx.demogit01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGit01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
